15e6782ffd2aba6a34b2fc169e94f2d9c2ddee25
marijaT13 <tashevska254@gmail.com>
2025-05-22 20:40:52 +0200

    Logout action

--- app/src/main/java/com/example/localist/MainActivity.java
+++ app/src/main/java/com/example/localist/MainActivity.java
@@ -2,13 +2,18 @@ package com.example.localist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 public class MainActivity extends AppCompatActivity {
 
+    private AppCompatButton logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,5 +21,14 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        logout = findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseAuth.getInstance().signOut();
+                Toast.makeText(MainActivity.this, "Logged out!", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(MainActivity.this, IntroActivity.class));
+            }
+        });
     }
 }
