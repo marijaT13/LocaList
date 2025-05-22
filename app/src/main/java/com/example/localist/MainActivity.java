0ae8224e3a268ed34804e4c78f908dec6f0e9a66
marijaT13 <tashevska254@gmail.com>
2025-05-22 19:23:53 +0200

    Focusable input on registery

--- app/src/main/java/com/example/localist/MainActivity.java
+++ app/src/main/java/com/example/localist/MainActivity.java
@@ -5,33 +5,18 @@ import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.AppCompatButton;
 
 
-import android.view.View;
-import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
 
-    private AppCompatButton register;
-    private AppCompatButton login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        register = findViewById(R.id.register);
-        login = findViewById(R.id.login);
-        register.setOnClickListener(v -> {
-            startActivity (new Intent(MainActivity.this, RegisterActivity.class));
-            finish();
-        });
 
-        login.setOnClickListener(v -> {
-            startActivity(new Intent(MainActivity.this, LoginActivity.class));
-            finish();
-        });
 
     }
 }
